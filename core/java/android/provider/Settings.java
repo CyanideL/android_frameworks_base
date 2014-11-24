5eb2dd93896411456f5f1e16eb4a9677bb40bebb
John Brewer <gzroms@hotmail.com>
2014-11-24 18:58:36 -0500

    AOKP custom system animations (1/2)
    Porting this to L to see how she works out
    
    Ported from here: http://gerrit.aokp.co/#/c/12304/
    
    Thanks to Steve Spear
    
    Change-Id: I4dcd14438120d1147dad30aa39619520cd366e05
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3623,8 +3623,29 @@ public final class Settings {
          * @hide
          */
         public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
-
+        
         /**
+         * Custom System Animations
+         *
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+        };
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
+        /**		
+>>>>>>> 123b935... AOKP custom system animations (1/2)
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
