8cafd47164bcfb23104d0021ab8f8e7ef2c4c128
snak3ater <saeedyasir91@gmail.com>
2014-11-30 14:09:58 -0200

    Framework: Add Fade in and Fade out effect to Recents FAB
    Credit: AragaoAnderson
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
    
    Change-Id: Ie371bc0b34cb670944c65dbbb700dbd9d96b3cca
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui.recents.views;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.ActivityOptions;
 import android.app.TaskStackBuilder;
 import android.content.ComponentName;
@@ -28,6 +30,7 @@ import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
+import android.view.ViewAnimationUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -413,8 +416,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
 
     public void startFABanimation() {
         // Animate the action button in
-        mFloatingButton = ((View)getParent()).findViewById(R.id.floating_action_button);
-        mFloatingButton.animate().alpha(1f)
+        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
+        mClearRecents.animate().alpha(1f)
                 .setStartDelay(mConfig.taskBarEnterAnimDelay)
                 .setDuration(mConfig.taskBarEnterAnimDuration)
                 .setInterpolator(mConfig.fastOutLinearInInterpolator)
@@ -424,8 +427,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
 
     public void endFABanimation() {
         // Animate the action button away
-        mFloatingButton = ((View)getParent()).findViewById(R.id.floating_action_button);
-        mFloatingButton.animate().alpha(0f)
+        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
+        mClearRecents.animate().alpha(0f)
                 .setStartDelay(0)
                 .setDuration(mConfig.taskBarExitAnimDuration)
                 .setInterpolator(mConfig.fastOutLinearInInterpolator)
