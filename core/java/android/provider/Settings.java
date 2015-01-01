85aeb03afa4182f0c8a44e0d308c7c0b72167957
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    [1/2] Quick settings: Add Haptic Feedback to tiles
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3647,6 +3647,12 @@ public final class Settings {
          */
         public static final String DISPLAY_TEMPERATURE_MODE = "display_temperature_mode";
 
+        /** Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
         /**
          * Automatic outdoor mode
          * @hide
