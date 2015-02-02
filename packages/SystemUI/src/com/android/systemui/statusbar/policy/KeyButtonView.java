2efaf0754d2beda7f5b299343d02bc5e34ef207e
Dave Kessler <activethrasher00@gmail.com>
2015-02-02 01:42:09 -0500

    KeyButtonView: We only need one instance of the audiomanager service
    Change-Id: I135b77d17d49658cebab66c99597b809dadf907e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -56,11 +56,12 @@ import java.util.ArrayList;
 
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.cyanide.KeyButtonInfo;
-import static com.android.internal.util.cyanide.NavbarConstants.*;
 import com.android.internal.util.cyanide.NavbarUtils;
 import com.android.internal.util.cyanide.CyanideActions;
 import com.android.systemui.R;
 import com.cyanide.util.DeviceUtils;
+
+import static com.android.internal.util.cyanide.NavbarConstants.*;
 
 import static android.view.accessibility.AccessibilityNodeInfo.ACTION_CLICK;
 import static android.view.accessibility.AccessibilityNodeInfo.ACTION_LONG_CLICK;
@@ -81,12 +82,12 @@ public class KeyButtonView extends ImageView {
     int mTouchSlop;
     private float mDrawingAlpha = 1f;
     private float mQuiescentAlpha = DEFAULT_QUIESCENT_ALPHA;
-    private AudioManager mAudioManager;
     private Animator mAnimateToQuiescent = new ObjectAnimator();
     private KeyButtonRipple mRipple;
     private boolean mShouldClick = true;
 
     private static PowerManager mPm;
+    private static AudioManager mAudioManager;
     KeyButtonInfo mActions;
 
     private boolean mIsDPadAction;
@@ -96,6 +97,11 @@ public class KeyButtonView extends ImageView {
     public static PowerManager getPowerManagerService(Context context) {
 		if (mPm == null) mPm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 		return mPm;
+	}
+
+    public static AudioManager getAudioManagerService(Context context) {
+		if (mAudioManager == null) mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		return mAudioManager;
 	}
 
     Runnable mCheckLongPress = new Runnable() {
@@ -127,9 +133,9 @@ public class KeyButtonView extends ImageView {
         mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
         mLongPressTimeout = ViewConfiguration.getLongPressTimeout();
         setLongClickable(false);
-        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        setBackground(new KeyButtonRipple(context, this));
+        mAudioManager = getAudioManagerService(context);
         mPm = getPowerManagerService(context);
+        setBackground(mRipple = new KeyButtonRipple(context, this));
     }
 
 	public void setButtonActions(KeyButtonInfo actions) {
