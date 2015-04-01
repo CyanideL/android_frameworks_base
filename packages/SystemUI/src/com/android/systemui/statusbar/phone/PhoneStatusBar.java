AICP/frameworks_base

FWB: Greeting label timeout (1/2)
Also include Themes persistance

Change-Id: I8e736cdbad733a66814d72bc108ce6116eec9e1a

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -364,6 +364,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private boolean mShowCarrierInPanel = false;
     private boolean mShowLabel;
+    private int mShowLabelTimeout;
 
     // Status bar carrier
     private boolean mShowStatusBarCarrier;
@@ -480,6 +481,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_GREETING),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -561,6 +565,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             if (mGreeting != null && !TextUtils.isEmpty(mGreeting)) {
                 mAicpLabel.setText(mGreeting);
             }
+
+            mShowLabelTimeout = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT, 400, mCurrentUserId);
         }
     }
 
@@ -2504,7 +2511,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mAicpLabel.animate().cancel();
                         mAicpLabel.animate()
                                 .alpha(1f)
-                                .setDuration(400)
+                                .setDuration(mShowLabelTimeout)
                                 .setInterpolator(ALPHA_IN)
                                 .setStartDelay(50)
                                 .withEndAction(new Runnable() {
@@ -4057,6 +4064,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mAicpLogo = Settings.System.getInt(
                     resolver, Settings.System.STATUS_BAR_AICP_LOGO, 0) == 1;
             showAicpLogo(mAicpLogo);
+
+            mGreeting = Settings.System.getStringForUser(resolver,
+                    Settings.System.STATUS_BAR_GREETING,
+                    UserHandle.USER_CURRENT);
+            if (mGreeting != null && !TextUtils.isEmpty(mGreeting)) {
+                mAicpLabel.setText(mGreeting);
+            }
+            mShowLabelTimeout = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT, 400,
+                    UserHandle.USER_CURRENT);
 
         } else {
             loadDimens();

CyanideL/android_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -374,6 +374,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private boolean mShowCarrierInPanel = false;
     private boolean mShowLabel;
+    private int mShowLabelTimeout;
 
     // battery
     private BatteryMeterView mBatteryView;
@@ -474,6 +475,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_GREETING),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -530,7 +534,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mCyanideLogo = Settings.System.getIntForUser(resolver,
                     Settings.System.STATUS_BAR_CYANIDE_LOGO, 0, mCurrentUserId) == 1;
             showCyanideLogo(mCyanideLogo);
-            
+
+            mGreeting = Settings.System.getStringForUser(resolver,
+                    Settings.System.STATUS_BAR_GREETING,
+                    UserHandle.USER_CURRENT);
+            if (mGreeting != null && !TextUtils.isEmpty(mGreeting)) {
+                mCyanideLabel.setText(mGreeting);
+            }
+
+            mShowLabelTimeout = Settings.System.getIntForUser(resolver,
+                    Settings.System.STATUS_BAR_GREETING_TIMEOUT, 500, mCurrentUserId);
         }
     }
     
@@ -2512,7 +2525,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mCyanideLabel.animate().cancel();
                         mCyanideLabel.animate()
                                 .alpha(1f)
-                                .setDuration(400)
+                                .setDuration(mShowLabelTimeout)
                                 .setInterpolator(ALPHA_IN)
                                 .setStartDelay(50)
                                 .withEndAction(new Runnable() {