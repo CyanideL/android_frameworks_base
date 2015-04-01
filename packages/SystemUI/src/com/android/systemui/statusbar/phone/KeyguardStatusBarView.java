b17bfe0589ac0b4319bb7ad614498a47468d69eb
rogersb11 <brettrogers11@gmail.com>
2015-04-01 12:15:05 -0400

    Derp

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -235,8 +235,4 @@ public class KeyguardStatusBarView extends RelativeLayout
     public boolean hasOverlappingRendering() {
         return false;
     }
-
-    public void updateCarrierLabelColor(int color) {
-        mCarrierLabel.setTextColor(color);
-    }
 }
