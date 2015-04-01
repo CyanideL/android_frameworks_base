fa19045da61b441a4724857852927a03db7fe858
rogersb11 <brettrogers11@gmail.com>
2015-04-01 06:44:15 -0400

    Revert "Framework: Add Fade in and Fade out effect to Recents FAB"
    This reverts commit 8cafd47164bcfb23104d0021ab8f8e7ef2c4c128.

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -16,8 +16,6 @@
 
 package com.android.systemui.recents.views;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.app.ActivityOptions;
 import android.app.TaskStackBuilder;
 import android.content.ComponentName;
@@ -30,7 +28,6 @@ import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
-import android.view.ViewAnimationUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -416,8 +413,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
 
     public void startFABanimation() {
         // Animate the action button in
-        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
-        mClearRecents.animate().alpha(1f)
+        mFloatingButton = ((View)getParent()).findViewById(R.id.floating_action_button);
+        mFloatingButton.animate().alpha(1f)
                 .setStartDelay(mConfig.taskBarEnterAnimDelay)
                 .setDuration(mConfig.taskBarEnterAnimDuration)
                 .setInterpolator(mConfig.fastOutLinearInInterpolator)
@@ -427,8 +424,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
 
     public void endFABanimation() {
         // Animate the action button away
-        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
-        mClearRecents.animate().alpha(0f)
+        mFloatingButton = ((View)getParent()).findViewById(R.id.floating_action_button);
+        mFloatingButton.animate().alpha(0f)
                 .setStartDelay(0)
                 .setDuration(mConfig.taskBarExitAnimDuration)
                 .setInterpolator(mConfig.fastOutLinearInInterpolator)
