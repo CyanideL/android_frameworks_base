VanirAOSP/frameworks_base

Squash of 11 of Dave's commits for Navi
Inception to 1/4/15.  For testing

Vanir actions and utilities [1/2]

This is a forward port of some helper utilities

Change-Id: I8e951b106d2a715096cb76555611d55056c06aee

Customizable Navbar [1/2]

Could use some updated drawables but good enough
for now.

Change-Id: Id8921ea4c654b4bf6293a1991ce6c49a539190e2
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	core/res/res/values/vanir_strings.xml
	core/res/res/values/vanir_symbols.xml
	packages/SystemUI/res/values/vanir_dimens.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

NavbarConstants: reduce metric shit-ton of unnecessary .value() calls

We can make these values static and import them from NavbarConstants and still
have them centrally located for "maintainability" without needing virtual accessor
methods everywhere under the sun.  Also some minor cleanups.

Change-Id: I0d9ef4b23ad035cd360f3a0febdc0ced9b81c347

navbar: make sure excluded list can't add additional blank items

Change-Id: Ie5673c5c5cb4db351b7e0f3a294fb4d8d2052004

NavigationBarView: correct hidden legacy menu visibility behavior

the menu key was changing into the layout changer when no additional layouts
were enabled which threw things off

Change-Id: I00f33669688b4da295bdda93d0ec91a655cbeee6
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java

whitespace: my text editor is a dickhead

Change-Id: I177ea90be558dcef62b55231a8c4bb22381ce523

NavigationBar: fix blank action

Change-Id: I6097b1affe134de63740d870dc394493249420f5

NavigationBarView: show layout changers with the keyboard up

Change-Id: I1de77fb75f62be0a5202011e4f6db7bfae810811

NavigationBarView: if there's no buttons lets revert to the default layout

I definitely had this in my source at some point but it must have been lost
in the shuffle.  If you don't want any keys on your navbar you might need a
bit of a slap... but regardless this p

core/java/com/android/internal/util/vanir/KeyButtonInfo.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2014 VanirAOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.vanir;
+
+import android.text.TextUtils;
+
+import static com.android.internal.util.vanir.NavbarConstants.*;
+
+public class KeyButtonInfo {
+    public static final String TAG = "keybuttoninfo";
+
+    public String singleAction, doubleTapAction, longPressAction, iconUri;
+
+    // reserved for single purpose keys
+    public KeyButtonInfo(String singleTap) {
+        this.singleAction = singleTap;
+    }
+
+    // reserved for configurable buttons
+    public KeyButtonInfo(String singleTap, String doubleTap, String longPress, String uri) {
+        this.singleAction = singleTap;
+        this.doubleTapAction = doubleTap;
+        this.longPressAction = longPress;
+        this.iconUri = uri;
+
+        if (singleAction != null) {
+            if ((singleAction.isEmpty()
+                    || singleAction.equals(ACTION_NULL))) {
+                singleAction = null;
+            }
+        }
+
+        if (doubleTapAction != null) {
+            if ((doubleTapAction.isEmpty()
+                    || doubleTapAction.equals(ACTION_NULL))) {
+                doubleTapAction = null;
+            }
+        }
+
+        if (longPressAction != null) {
+            if ((longPressAction.isEmpty()
+                    || longPressAction.equals(ACTION_NULL))) {
+                longPressAction = null;
+            }
+        }
+	}
+}

core/java/com/android/internal/util/vanir/NavbarUtils.java
@@ -1,0 +1,143 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.vanir;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import com.android.internal.util.vanir.NavbarConstants;
+import static com.android.internal.util.vanir.NavbarConstants.*;
+import com.android.internal.util.vanir.NavbarConstants.NavbarConstant;
+
+public class NavbarUtils {
+	private static final String TAG = NavbarUtils.class.getSimpleName();
+
+    // These items are excluded from settings and cannot be set as targets
+    private static final String[] EXCLUDED_FROM_NAVBAR = {
+            ACTION_RING_SILENT,
+            ACTION_RING_VIB,
+            ACTION_RING_VIB_SILENT,
+            ACTION_NULL,
+            ACTION_POWER,
+            ACTION_LAYOUT_LEFT,
+            ACTION_LAYOUT_RIGHT,
+            ACTION_ARROW_LEFT,
+            ACTION_ARROW_RIGHT,
+            ACTION_ARROW_UP,
+            ACTION_ARROW_DOWN,
+            ACTION_TORCH,
+            ACTION_IME_LAYOUT
+    };
+
+    private NavbarUtils() {
+    }
+
+    public static Drawable getIconImage(Context mContext, String uri) {
+        Drawable actionIcon;
+
+        if (TextUtils.isEmpty(uri)) {
+			uri = ACTION_NULL;
+		}
+
+        if (uri.startsWith("**")) {
+            return NavbarConstants.getActionIcon(mContext, uri);
+        } else {  // This must be an app 
+            try {
+                actionIcon = mContext.getPackageManager().getActivityIcon(Intent.parseUri(uri, 0));
+            } catch (NameNotFoundException e) {
+                e.printStackTrace();
+                actionIcon = NavbarConstants.getActionIcon(mContext,
+                        ACTION_NULL);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                actionIcon = NavbarConstants.getActionIcon(mContext,
+                        ACTION_NULL);
+            }
+        }
+        return actionIcon;
+    }
+
+    public static String[] getNavBarActions(Context context) {
+        ArrayList<String> mActionsArray = new ArrayList<String>();
+        String[] fullActionStringArray = NavbarConstants.NavbarActions();
+        // Perfection is achieved, not when there is nothing more to add,
+        // but when there is nothing left to take away. --Antoine de Saint-Exupéry, Airman's Odyssey 
+        for (String action : fullActionStringArray) {
+            if (Arrays.asList(EXCLUDED_FROM_NAVBAR).contains(action)) continue;
+            mActionsArray.add(action);
+        }
+        String[] mActions = new String[mActionsArray.size()];
+        mActions = mActionsArray.toArray(mActions);
+        return mActions;
+    }
+
+    public static String getProperSummary(Context mContext, String uri) {
+		if (TextUtils.isEmpty(uri)) {
+			uri = ACTION_NULL;
+		}
+
+        if (uri.startsWith("**")) {
+            return NavbarConstants.getProperName(mContext, uri);
+        } else {  // This must be an app
+            try {
+                Intent intent = Intent.parseUri(uri, 0);
+                if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+                    return getFriendlyActivityName(mContext, intent);
+                }
+                return getFriendlyShortcutName(mContext, intent);
+            } catch (URISyntaxException e) {
+                return NavbarConstants.getProperName(mContext, ACTION_NULL);
+            }
+        }
+    }
+
+    private static String getFriendlyActivityName(Context mContext, Intent intent) {
+        PackageManager pm = mContext.getPackageManager();
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null) {
+                friendlyName = ai.name;
+            }
+        }
+
+        return (friendlyName != null) ? friendlyName : intent.toUri(0);
+    }
+
+    private static String getFriendlyShortcutName(Context mContext, Intent intent) {
+        String activityName = getFriendlyActivityName(mContext, intent);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+}

CyanideL/android_frameworks_base

	core/java/android/provider/Settings.java
	core/java/com/android/internal/statusbar/IStatusBar.aidl
	core/res/res/values-mcc415-mnc01/config.xml
	packages/DefaultContainerService/res/values-ast-rES/strings.xml
	packages/DefaultContainerService/res/values-lb/strings.xml
	packages/SystemUI/res/layout/navigation_bar.xml
	packages/SystemUI/res/values/vanir_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java

core/java/com/android/internal/util/cyanide/KeyButtonInfo.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2014 VanirAOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.cyanide;
+
+import android.text.TextUtils;
+
+import static com.android.internal.util.cyanide.NavbarConstants.*;
+
+public class KeyButtonInfo {
+    public static final String TAG = "keybuttoninfo";
+
+    public String singleAction, doubleTapAction, longPressAction, iconUri;
+
+    // reserved for single purpose keys
+    public KeyButtonInfo(String singleTap) {
+        this.singleAction = singleTap;
+    }
+
+    // reserved for configurable buttons
+    public KeyButtonInfo(String singleTap, String doubleTap, String longPress, String uri) {
+        this.singleAction = singleTap;
+        this.doubleTapAction = doubleTap;
+        this.longPressAction = longPress;
+        this.iconUri = uri;
+
+        if (singleAction != null) {
+            if ((singleAction.isEmpty()
+                    || singleAction.equals(ACTION_NULL))) {
+                singleAction = null;
+            }
+        }
+
+        if (doubleTapAction != null) {
+            if ((doubleTapAction.isEmpty()
+                    || doubleTapAction.equals(ACTION_NULL))) {
+                doubleTapAction = null;
+            }
+        }
+
+        if (longPressAction != null) {
+            if ((longPressAction.isEmpty()
+                    || longPressAction.equals(ACTION_NULL))) {
+                longPressAction = null;
+            }
+        }
+	}
+}

core/java/com/android/internal/util/cyanide/NavbarUtils.java
@@ -1,0 +1,143 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.cyanide;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import com.android.internal.util.cyanide.NavbarConstants;
+import static com.android.internal.util.cyanide.NavbarConstants.*;
+import com.android.internal.util.cyanide.NavbarConstants.NavbarConstant;
+
+public class NavbarUtils {
+	private static final String TAG = NavbarUtils.class.getSimpleName();
+
+    // These items are excluded from settings and cannot be set as targets
+    private static final String[] EXCLUDED_FROM_NAVBAR = {
+            ACTION_RING_SILENT,
+            ACTION_RING_VIB,
+            ACTION_RING_VIB_SILENT,
+            ACTION_NULL,
+            ACTION_POWER,
+            ACTION_LAYOUT_LEFT,
+            ACTION_LAYOUT_RIGHT,
+            ACTION_ARROW_LEFT,
+            ACTION_ARROW_RIGHT,
+            ACTION_ARROW_UP,
+            ACTION_ARROW_DOWN,
+            ACTION_TORCH,
+            ACTION_IME_LAYOUT
+    };
+
+    private NavbarUtils() {
+    }
+
+    public static Drawable getIconImage(Context mContext, String uri) {
+        Drawable actionIcon;
+
+        if (TextUtils.isEmpty(uri)) {
+			uri = ACTION_NULL;
+		}
+
+        if (uri.startsWith("**")) {
+            return NavbarConstants.getActionIcon(mContext, uri);
+        } else {  // This must be an app 
+            try {
+                actionIcon = mContext.getPackageManager().getActivityIcon(Intent.parseUri(uri, 0));
+            } catch (NameNotFoundException e) {
+                e.printStackTrace();
+                actionIcon = NavbarConstants.getActionIcon(mContext,
+                        ACTION_NULL);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                actionIcon = NavbarConstants.getActionIcon(mContext,
+                        ACTION_NULL);
+            }
+        }
+        return actionIcon;
+    }
+
+    public static String[] getNavBarActions(Context context) {
+        ArrayList<String> mActionsArray = new ArrayList<String>();
+        String[] fullActionStringArray = NavbarConstants.NavbarActions();
+        // Perfection is achieved, not when there is nothing more to add,
+        // but when there is nothing left to take away. --Antoine de Saint-Exupéry, Airman's Odyssey 
+        for (String action : fullActionStringArray) {
+            if (Arrays.asList(EXCLUDED_FROM_NAVBAR).contains(action)) continue;
+            mActionsArray.add(action);
+        }
+        String[] mActions = new String[mActionsArray.size()];
+        mActions = mActionsArray.toArray(mActions);
+        return mActions;
+    }
+
+    public static String getProperSummary(Context mContext, String uri) {
+		if (TextUtils.isEmpty(uri)) {
+			uri = ACTION_NULL;
+		}
+
+        if (uri.startsWith("**")) {
+            return NavbarConstants.getProperName(mContext, uri);
+        } else {  // This must be an app
+            try {
+                Intent intent = Intent.parseUri(uri, 0);
+                if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+                    return getFriendlyActivityName(mContext, intent);
+                }
+                return getFriendlyShortcutName(mContext, intent);
+            } catch (URISyntaxException e) {
+                return NavbarConstants.getProperName(mContext, ACTION_NULL);
+            }
+        }
+    }
+
+    private static String getFriendlyActivityName(Context mContext, Intent intent) {
+        PackageManager pm = mContext.getPackageManager();
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null) {
+                friendlyName = ai.name;
+            }
+        }
+
+        return (friendlyName != null) ? friendlyName : intent.toUri(0);
+    }
+
+    private static String getFriendlyShortcutName(Context mContext, Intent intent) {
+        String activityName = getFriendlyActivityName(mContext, intent);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+}