437fc6d3dbc8c5b3c984c224fc29ed6acb602b78
rogersb11 <brettrogers11@gmail.com>
2015-04-14 15:36:59 -0400

    Revert "perf: Boost during animations"
    This reverts commit 99106dead8e09b6e7366ac872d243ba5ee0b13dc.

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -1713,9 +1713,6 @@ final class ActivityStack {
                     mWindowManager.prepareAppTransition(prev.task == next.task
                             ? AppTransition.TRANSIT_ACTIVITY_CLOSE
                             : AppTransition.TRANSIT_TASK_CLOSE, false);
-                    if (prev.task != next.task) {
-                        mStackSupervisor.mPm.cpuBoost(2000 * 1000);
-                    }
                 }
                 mWindowManager.setAppWillBeHidden(prev.appToken);
                 mWindowManager.setAppVisibility(prev.appToken, false);
@@ -1730,9 +1727,6 @@ final class ActivityStack {
                             : next.mLaunchTaskBehind
                                     ? AppTransition.TRANSIT_TASK_OPEN_BEHIND
                                     : AppTransition.TRANSIT_TASK_OPEN, false);
-                    if (prev.task != next.task) {
-                        mStackSupervisor.mPm.cpuBoost(2000 * 1000);
-                    }
                 }
             }
             if (false) {
