b4e7aa9915fe31a9fdae1efd1c0099f7c439f966
Daniel Koman <dankoman30@gmail.com>
2015-05-17 03:12:08 -0600

    Add Feature: Heads Up custom text/background color/transparency
    Ported by Team Bliss && @rogerb11
    
    Change-Id: I40e5b773265e2a358aa47729c4a3a9fe34b53817
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -19,6 +19,7 @@ package com.android.systemui.statusbar.policy;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Outline;
@@ -55,6 +56,7 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
     private static final String TAG = "HeadsUpNotificationView";
     private static final boolean DEBUG = false;
     private static final boolean SPEW = DEBUG;
+    private final int HEADSUP_DEFAULT_BACKGROUNDCOLOR = 0x00ffffff;
 
     Rect mTmpRect = new Rect();
     int[] mTmpTwoArray = new int[2];
@@ -73,6 +75,8 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
     private int mSnoozeLengthMs;
     private boolean mAttached = false;
     private ContentObserver mSettingsObserver;
+
+    private int mBackground;
 
     private NotificationData.Entry mHeadsUp;
     private int mUser;
@@ -94,6 +98,10 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
         mSnoozedPackages = new ArrayMap<>();
         sRoundedRectCornerRadius = context.getResources().getDimensionPixelSize(
                 R.dimen.notification_material_rounded_rect_radius);
+        mContext = context;
+        mBackground = Settings.System.getIntForUser(
+            mContext.getContentResolver(), Settings.System.HEADS_UP_BG_COLOR,
+            HEADSUP_DEFAULT_BACKGROUNDCOLOR, UserHandle.USER_CURRENT);
     }
 
     class SettingsObserver extends ContentObserver {
@@ -144,13 +152,14 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
         return mContentHolder;
     }
 
-    public boolean showNotification(NotificationData.Entry headsUp) {
+    public boolean showNotification(NotificationData.Entry headsUp, int background) {
         if (mHeadsUp != null && headsUp != null && !mHeadsUp.key.equals(headsUp.key)) {
             // bump any previous heads up back to the shade
             release();
         }
 
         mHeadsUp = headsUp;
+        mBackground = background;
 
         if (mBar.isExpandedVisible() || mBar.isImeShowing()) {
             releaseAndClose();
@@ -163,6 +172,13 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
             // too soon! Reparent and exit here.
             releaseAndClose();
             return false;
+        }
+
+        // set custom background
+        if (mBackground != HEADSUP_DEFAULT_BACKGROUNDCOLOR) {
+            setHeadsUpCustomBg();
+        } else {
+            setHeadsUpDefaultBg();
         }
 
         mTouchOutside = false;
@@ -184,6 +200,13 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
             mStartTouchTime = SystemClock.elapsedRealtime() + mTouchSensitivityDelay;
 
             mHeadsUp.setInterruption();
+
+           // set content holder background based on whether notification
+           // color is custom or default
+           mContentHolder.setBackgroundResource(0);
+           if (mBackground == HEADSUP_DEFAULT_BACKGROUNDCOLOR) {
+               mContentHolder.setBackgroundResource(R.drawable.heads_up_window_bg);
+           }
 
             // 2. Animate mHeadsUpNotificationView in
             mBar.scheduleHeadsUpOpen();
@@ -275,6 +298,28 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
         return mHeadsUp == null || mHeadsUp.notification.isClearable();
     }
 
+    private void setHeadsUpCustomBg() {
+            View expanded = mHeadsUp.expanded;
+            View expandedBig = mHeadsUp.getBigContentView();
+            if (expanded !=null) {
+                expanded.setBackgroundColor(mBackground);
+            }
+            if (expandedBig != null) {
+                expandedBig.setBackgroundColor(mBackground);
+            }
+    }
+
+    private void setHeadsUpDefaultBg() {
+            View expanded = mHeadsUp.expanded;
+            View expandedBig = mHeadsUp.getBigContentView();
+            if (expanded !=null) {
+                expanded.setBackgroundColor(0x00000000);
+            }
+            if (expandedBig != null) {
+                expandedBig.setBackgroundColor(0x00000000);
+            }
+    }
+
     // ViewGroup methods
 
     private static final ViewOutlineProvider CONTENT_HOLDER_OUTLINE_PROVIDER =
@@ -309,16 +354,19 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
             mContentHolder = (ViewGroup) findViewById(R.id.content_holder);
             mContentHolder.setOutlineProvider(CONTENT_HOLDER_OUTLINE_PROVIDER);
 
+            mBackground = Settings.System.getIntForUser(
+            mContext.getContentResolver(), Settings.System.HEADS_UP_BG_COLOR,
+            HEADSUP_DEFAULT_BACKGROUNDCOLOR, UserHandle.USER_CURRENT);
+            
             if (mSettingsObserver == null) {
                 mSettingsObserver = new SettingsObserver(new Handler());
+
             }
 
-            if (DEBUG) Log.v(TAG, "mSnoozeLengthMs = " + mSnoozeLengthMs);
-
-            if (mHeadsUp != null) {
-                // whoops, we're on already!
-                showNotification(mHeadsUp);
-            }
+        if (mHeadsUp != null) {
+            // whoops, we're on already!
+            showNotification(mHeadsUp, mBackground);
+        }
 
             getViewTreeObserver().addOnComputeInternalInsetsListener(this);
 
