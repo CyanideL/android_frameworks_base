3592fc3923b5cd2fccb215acc30c7714d35d1758
Alex Cruz <mazdarider23@gmail.com>
2015-05-25 15:01:25 +0200

    CAF Task Manager switch [1/2]
    Change-Id: I4b7c3041bfa74bcd7e2981a39a34860422f596fc
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5185,6 +5185,12 @@ public final class Settings {
         public static final String NOTIFICATION_CLEAR_ALL_ICON_COLOR = "notification_clear_all_icon_color";
 
         /**
+         * Enable built in CAF task manager
+         * @hide
+         */
+        public static final String ENABLE_TASK_MANAGER = "enable_task_manager";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
