7dcd065781aa8f1f4895446c2da04723bb0e4fd6
Matt Garnes <matt@cyngn.com>
2015-06-23 12:03:58 -0700

    Add default for Settings.System.DOUBLE_TAP_SLEEP_GESTURE.
    Change-Id: I35240df67402d30117f6b8522b80da08476b43c0
    
    Conflicts:
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2575,6 +2575,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.System.STATUS_BAR_SHOW_NOTIF_COUNT,
                     R.integer.def_notif_count);
 
+            loadIntegerSetting(stmt, Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
+                    R.integer.def_double_tap_sleep_gesture);
+
             loadHeadsUpSetting(stmt);
 
             loadIntegerSetting(stmt, Settings.System.ENABLE_FORWARD_LOOKUP,
