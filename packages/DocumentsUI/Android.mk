5f848f340d8de44ac9432fe16fd5fb75258a3fc9
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-08-13 20:15:45 +0200

    DocumentsUI: TRDS Material Dark.
    PS: Also include "Fix aapt removing the darktheme resources"
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- packages/DocumentsUI/Android.mk
+++ packages/DocumentsUI/Android.mk
@@ -7,6 +7,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 guava
 
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+
 LOCAL_PACKAGE_NAME := DocumentsUI
 LOCAL_CERTIFICATE := platform
 
