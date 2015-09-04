12d4165295a163fdb07b12ddff7da30b63056f3c
rogersb11 <brettrogers11@gmail.com>
2015-09-04 04:10:56 -0400

    FWB:Status bar carrier label font size 2/2

--- packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java
@@ -47,6 +47,7 @@ public class CarrierLabel extends TextView {
     private Context mContext;
     private boolean mAttached;
     private static boolean isCN;
+    private int mCarrierFontSize = 14;
 
     protected int mCarrierColor = getResources().getColor(R.color.kg_carrier_text);
     Handler mHandler;
@@ -60,11 +61,14 @@ public class CarrierLabel extends TextView {
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUS_BAR_CARRIER_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_FONT_SIZE), false, this, UserHandle.USER_CURRENT);
         }
 
         @Override
         public void onChange(boolean selfChange) {
             updateColor();
+            updateSize();
         }
     }
 
@@ -84,6 +88,7 @@ public class CarrierLabel extends TextView {
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
         settingsObserver.observe();
         updateColor();
+        updateSize();
     }
 
     @Override
@@ -136,6 +141,7 @@ public class CarrierLabel extends TextView {
         }
         String customCarrierLabel = Settings.System.getStringForUser(mContext.getContentResolver(),
                 Settings.System.CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
+
         if (!TextUtils.isEmpty(customCarrierLabel)) {
             setText(customCarrierLabel);
         } else {
@@ -173,5 +179,13 @@ public class CarrierLabel extends TextView {
              mCarrierColor = newColor;
         }
         setTextColor(mCarrierColor);
+    }
+
+    private void updateSize() {
+        mCarrierFontSize = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_FONT_SIZE, 14,
+                UserHandle.USER_CURRENT);
+
+        setTextSize(mCarrierFontSize);
     }
 }
