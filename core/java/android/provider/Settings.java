62a78c842d2f68d6c4d0026f6ba38c17e08158ff
beanstown106 <nbenis106@gmail.com>
2015-09-12 10:18:07 -0400

    Disable Lockscreen Media Art [1/2] * toggle to choose whether or not to show album art/media art on lockscreen
    Special thanks to @atl4tis from Cataclysm rom for pointing out where to hook into this
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6107,6 +6107,12 @@ public final class Settings {
         public static final String TASK_MANAGER_APP_COLOR = "task_manager_app_color";
 
         /**
+         * Whether to show media art on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
