BenzoRoms/frameworks_base

Status Bar Logo 2/2
Original idea goes to aicp but I recoded everything and made some improvements
Color for both light and dark modes
Location: Left or Right
Options to show in Status bar and lock screen
Options to hide the logo when a certain number of notification icons are in the status bar

Change-Id: Ib5ebfde2f23f07e79cda71d24526a1f19c80ef7c
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

core/java/android/provider/Settings.java
@@ -4633,6 +4633,57 @@ public final class Settings {
         public static final String SHOW_FOURG = "show_fourg";
 
         /**
+         * Statusbar Benzo logo
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_SHOW = "status_bar_benzo_logo_show";
+
+        /**
+         * Statusbar Benzo logo on the lock screen
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_SHOW_ON_LOCK_SCREEN = "status_bar_benzo_logo_show_on_lock_screen";
+
+        /**
+         * Style of Status bar logo
+         * 0 - Left (default)
+         * 1 - Right
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_STYLE = "status_bar_benzo_logo_style";
+
+        /**
+         * Wether to hide the Benzo logo on the status bar when the custom number of notifications is reached
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_HIDE_LOGO = "status_bar_benzo_logo_hide_logo";
+
+        /**
+         * Hide the Benzo logo on the status bar when the notification icons will reach this number
+         * 1: 1 notification icon (always)
+         * 2: 2 notification icons
+         * 3: 3 notification icons
+         * 4: 4 notification icons
+         * 5: 5 notification icons
+         * 6: 6 notification icons
+         * default: 1
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_NUMBER_OF_NOTIFICATION_ICONS = "status_bar_benzo_logo_number_of_notification_icons";
+
+        /**
+         * Statusbar Benzo logo color setting
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_COLOR = "status_bar_benzo_logo_color";
+
+        /**
+         * Statusbar Benzo logo color in dark mode
+         * @hide
+         */
+        public static final String STATUS_BAR_BENZO_LOGO_COLOR_DARK_MODE = "status_bar_benzo_logo_color_dark_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -33,7 +33,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mClock, mNetworkTraffic;
+    private View mBenzoLogoLeft, mLeftSide, mStatusIcons, mSignalCluster, mClock, mNetworkTraffic, mBenzoLogo;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -44,12 +44,14 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     }
 
     public void init() {
+        mBenzoLogoLeft = mView.findViewById(R.id.left_benzo_logo);
         mLeftSide = mView.findViewById(R.id.notification_icon_area);
         mStatusIcons = mView.findViewById(R.id.statusIcons);
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         //mBattery = mView.findViewById(R.id.battery);
         mClock = mView.findViewById(R.id.clock);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
+        mBenzoLogo = mView.findViewById(R.id.benzo_logo);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -90,12 +92,14 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         if (animate) {
             AnimatorSet anims = new AnimatorSet();
             anims.playTogether(
+                    animateTransitionTo(mBenzoLogoLeft, newAlphaBC),
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mSignalCluster, newAlpha),
                     animateTransitionTo(mNetworkTraffic, newAlpha),
                     //animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC)
+                    animateTransitionTo(mClock, newAlphaBC),
+                    animateTransitionTo(mBenzoLogo, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -103,12 +107,14 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
+            mBenzoLogoLeft.setAlpha(newAlphaBC);
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mSignalCluster.setAlpha(newAlpha);
             mNetworkTraffic.setAlpha(newAlpha);
 //            mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
+            mBenzoLogo.setAlpha(newAlphaBC);
         }
     }
 }

CyanideL/android_frameworks_base

core/java/android/provider/Settings.java
@@ -4425,6 +4425,57 @@ public final class Settings {
         public static final String STATUS_BAR_CLOCK_FONT_SIZE = "status_bar_clock_font_size";
 
         /**
+		 * Statusbar CyanideL logo
+		 * @hide
+		 */
+		public static final String STATUS_BAR_CYANIDE_LOGO_SHOW = "status_bar_cyanide_logo_show";
+
+        /**
+		 * Statusbar CyanideL logo on the lock screen
+		 * @hide
+		 */
+		public static final String STATUS_BAR_CYANIDE_LOGO_SHOW_ON_LOCK_SCREEN = "status_bar_cyanide_logo_show_on_lock_screen";
+
+        /**
+		 * Style of Status bar logo
+         * 0 - Left (default)
+         * 1 - Right
+         * @hide
+		 */
+		public static final String STATUS_BAR_CYANIDE_LOGO_STYLE = "status_bar_cyanide_logo_style";
+
+        /**
+         * Wether to hide the Cyanide logo on the status bar when the custom number of notifications is reached
+         * @hide
+         */
+        public static final String STATUS_BAR_CYANIDE_LOGO_HIDE_LOGO = "status_bar_cyanide_logo_hide_logo";
+
+        /**
+         * Hide the Cyanide logo on the status bar when the notification icons will reach this number
+         * 1: 1 notification icon (always)
+         * 2: 2 notification icons
+         * 3: 3 notification icons
+         * 4: 4 notification icons
+         * 5: 5 notification icons
+         * 6: 6 notification icons
+         * default: 1
+         * @hide
+         */
+        public static final String STATUS_BAR_CYANIDE_LOGO_NUMBER_OF_NOTIFICATION_ICONS = "status_bar_cyanide_logo_number_of_notification_icons";
+
+        /**
+         * Statusbar CyanideL logo color setting
+         * @hide
+         */
+        public static final String STATUS_BAR_CYANIDE_LOGO_COLOR = "status_bar_cyanide_logo_color";
+
+        /**
+		 * Statusbar CyanideL logo color in dark mode
+		 * @hide
+		 */
+		public static final String STATUS_BAR_CYANIDE_LOGO_COLOR_DARK_MODE = "status_bar_cyanide_logo_color_dark_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock;
+    private View mCyanideLogoLeft, mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCyanideLogo;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -43,11 +43,13 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     }
 
     public void init() {
+        mCyanideLogoLeft = mView.findViewById(R.id.left_cyanide_logo);
         mLeftSide = mView.findViewById(R.id.notification_icon_area);
         mStatusIcons = mView.findViewById(R.id.statusIcons);
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         mBattery = mView.findViewById(R.id.battery);
         mClock = mView.findViewById(R.id.clock);
+        mCyanideLogo = mView.findViewById(R.id.cyanide_logo);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -88,11 +90,13 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         if (animate) {
             AnimatorSet anims = new AnimatorSet();
             anims.playTogether(
+                    animateTransitionTo(mCyanideLogoLeft, newAlphaBC),
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mSignalCluster, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC)
+                    animateTransitionTo(mClock, newAlphaBC),
+                    animateTransitionTo(mCyanideLogo, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -100,11 +104,13 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
+            mCyanideLogoLeft.setAlpha(newAlphaBC);
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mSignalCluster.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
+            mCyanideLogo.setAlpha(newAlphaBC);
         }
     }
 }