AICP/frameworks_base

FWB: AE custom summary in Settings app (1/3)
Change-Id: I53ecff85793fc3673db5bd90d54989c561351c89

core/java/android/provider/Settings.java
@@ -3737,6 +3737,12 @@ public final class Settings {
         public static final String AE_NAV_DRAWER_CHECKED_TEXT= "ae_nav_drawer_checked_text";
 
         /**
+          * AE Settings activity custom summary text
+          * @hide
+          */
+        public static final String AE_SETTINGS_SUMMARY= "ae_settings_summary";
+
+        /**
          * Whether to show the battery bar
          * @hide
          */

CyanideL/android_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java

core/java/android/provider/Settings.java
@@ -5129,6 +5129,12 @@ public final class Settings {
 		public static final String STATUS_BAR_CUSTOM_LABEL_TEXT_COLOR_DARK_MODE = "status_bar_custom_label_text_color_dark_mode";
 
         /**
+          * Cyanide Settings activity custom summary text
+          * @hide
+          */
+        public static final String CM_SETTINGS_SUMMARY= "cm_settings_summary";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *