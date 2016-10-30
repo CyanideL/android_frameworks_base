69f2de16c46a295e380c995e82095a9d41bd236a
bigrushdog <randall.rushing@gmail.com>
2016-10-29 22:45:53 -0700

    DUI: set bar IME hints when we add/change bar
    Outlier usecase where user shows/changes bar (likely with nav tile)
    while IME is showing. SmartBar won't be in a IME-showing state
    (and in theory Fling if we implement IME status)
    
    Change-Id: I2c1ba69de498b128b1be20dfb984739be6a106ec

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1578,6 +1578,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mNavigationController.getBar().getBaseView().setVisibility(View.GONE);
         }
         mNavigationController.getBar().notifyInflateFromUser();
+        mNavigationController.getBar().setNavigationIconHints(mNavigationIconHints);
         addNavigationBar();
     }
 
