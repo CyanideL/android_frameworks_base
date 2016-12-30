7afb0003b1987d22bcbb8f608efe0790a79aff21
Ali B <abittin@gmail.com>
2016-12-30 18:52:26 +0300

    QS: Add Pulse Visualizer Tile
    PS2-3 Cleanup white space
    PS4 Update commit message
    
    Thanks to @varund7726 for drawables
    
    Change-Id: I6e4074adaa5196b3c47f610bd0bb4d35c85ec84d
    
    Conflicts:
    	packages/SystemUI/res/values/aicp_strings.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.MusicTile;
+import com.android.systemui.qs.tiles.PulseTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -450,6 +451,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals("volume")) return new VolumeTile(this);
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
+        else if (tileSpec.equals("pulse")) return new PulseTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
